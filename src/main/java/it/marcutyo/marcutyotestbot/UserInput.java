f06f18ad270867a669c96fcd52b01cc74644996a
Marcutyo <78793610+Marcutyo@users.noreply.github.com>
2021-10-01 17:22:57 +0200

    Constructor checks validity of message and if the message text is a command

--- src/main/java/it/marcutyo/marcutyotestbot/UserInput.java
+++ src/main/java/it/marcutyo/marcutyotestbot/UserInput.java
@@ -5,31 +5,44 @@ import org.telegram.telegrambots.meta.api.objects.Update;
 import org.telegram.telegrambots.meta.api.objects.User;
 
 public class UserInput {
-    private boolean command;
-    private String verb;
+    private String command = "help";
+    private String[] arguments = new String[0];
     private String chatId;
     private User user;
 
-    public UserInput(Update update) {
-        String data = getData(update);
-        if (data.startsWith("/")) this.command = true;
-        this.verb = data;
-    }
 
-    private String getData(Update update) {
+    public UserInput(Update update) {
         if (update.hasMessage()) {
             Message message = update.getMessage();
-            setIds(message);
-            return message.getText();
-        } else {
-            Message message = update.getCallbackQuery().getMessage();
-            setIds(message);
-            return update.getCallbackQuery().getData();
+            setChatId(message);
+            setUser(message);
+
+            if (message.hasText() && message.getText().startsWith("/")) {
+                String[] messageText = message.getText().split("\\s+");
+                setCommand(messageText);
+                setArguments(messageText);
+            }
         }
     }
 
-    private void setIds(Message message) {
+    private void setChatId(Message message) {
         this.chatId = message.getChatId().toString();
+    }
+
+    private void setUser(Message message) {
         this.user = message.getFrom();
+    }
+
+    private void setCommand(String[] messageText) {
+        this.command = messageText[0].replace("/", "");
+    }
+
+    private void setArguments(String[] messageText) {
+        if (messageText.length>1) {
+            arguments = new String[messageText.length - 1];
+            for (short i = 1; i < messageText.length; i++) {
+                arguments[i] = messageText[i];
+            }
+        }
     }
 }
