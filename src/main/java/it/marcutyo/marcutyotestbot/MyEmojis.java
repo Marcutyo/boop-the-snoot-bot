18206880ecf7f194fde45021fdbded757c68cf73
Marcutyo <78793610+Marcutyo@users.noreply.github.com>
2021-10-04 19:38:03 +0200

    Created static class to avoid ugly unicode implementation of emojis

---
+++ src/main/java/it/marcutyo/marcutyotestbot/MyEmojis.java
@@ -0,0 +1,9 @@
+package it.marcutyo.marcutyotestbot;
+
+public final class MyEmojis {
+    private MyEmojis(){}
+
+    public static String IT_FLAG = "\uD83C\uDDEE\uD83C\uDDF9";
+    public static String UK_FLAG = "\uD83C\uDDEC\uD83C\uDDE7";
+    public static String US_FLAG = "\uD83C\uDDFA\uD83C\uDDF8";
+}
