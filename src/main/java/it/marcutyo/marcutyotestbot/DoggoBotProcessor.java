9b0b5bb359f48b83d50d353c7d0af201ce4c54a3
Marcutyo <78793610+Marcutyo@users.noreply.github.com>
2021-10-05 15:17:49 +0200

    Processes the commands (stub)

--- src/main/java/it/marcutyo/marcutyotestbot/DoggoBotProcessor.java
+++ src/main/java/it/marcutyo/marcutyotestbot/DoggoBotProcessor.java
@@ -1,11 +1,9 @@
 package it.marcutyo.marcutyotestbot;
 
 import lombok.Getter;
-import lombok.SneakyThrows;
 import org.springframework.stereotype.Component;
 import org.telegram.telegrambots.extensions.bots.commandbot.commands.BotCommand;
 import org.telegram.telegrambots.extensions.bots.commandbot.commands.helpCommand.HelpCommand;
-import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.objects.Chat;
 import org.telegram.telegrambots.meta.api.objects.User;
 import org.telegram.telegrambots.meta.bots.AbsSender;
@@ -13,30 +11,65 @@ import org.telegram.telegrambots.meta.bots.AbsSender;
 @Getter
 @Component
 public class DoggoBotProcessor {
-    private final BotCommand ECHO_COMMAND;
-    private final HelpCommand HELP2_COMMAND;
-    private final BotCommand DEFAULT_COMMAND;
+    private final HelpCommand HELP_COMMAND;
 
+    private final BotCommand BOOP_PIC_COMMAND;
+    private final BotCommand BOOP_VID_COMMAND;
+    private final BotCommand BOOP_STICKER_COMMAND;
+    private final BotCommand BOOP_THE_SNOOT;
 
     public DoggoBotProcessor() {
-        this.ECHO_COMMAND = new BotCommand("echo", "I'll repeat everything you write") {
-            @SneakyThrows
+
+        this.HELP_COMMAND = new HelpCommand(
+                "help",
+                "🇮🇹 per avere un riepilogo dettagliato dei comandi ❓\n" +
+                        "🇬🇧🇺🇸 to get a detailed summary of commands",
+                "HelpCommand extended description placeholder"
+        );
+
+        this.BOOP_PIC_COMMAND = new BotCommand(
+                "boop_pic",
+                "🇮🇹 per ricevere una immagine casuale di un 𝒹𝑜𝑔𝑔𝑜 🐕‍🦺\n" +
+                        "🇬🇧🇺🇸 to get a random 𝒹𝑜𝑔𝑔𝑜 pic"
+        ) {
             @Override
             public void execute(AbsSender absSender, User user, Chat chat, String[] strings) {
-                String str = String.join(" ", strings);
-                absSender.execute(new SendMessage(chat.getId().toString(), str));
+
             }
         };
 
-        this.DEFAULT_COMMAND = new BotCommand("default", "È il metodo default") {
-            @SneakyThrows
+        this.BOOP_VID_COMMAND = new BotCommand(
+                "boop_vid",
+                "🇮🇹 per ricevere una GIF casuale di un 𝒹𝑜𝑔𝑔𝑜 🐕\n" +
+                        "🇬🇧🇺🇸 to get a random 𝒹𝑜𝑔𝑔𝑜 gif"
+        ) {
             @Override
             public void execute(AbsSender absSender, User user, Chat chat, String[] strings) {
-                absSender.execute(new SendMessage(chat.getId().toString(), "Messaggio default se il metodo non esiste o non è un metodo"));
+
             }
         };
 
-        this.HELP2_COMMAND = new HelpCommand("help", "mostra tutti i comandi", "è un comando fantastico per farti avere tutto maremma");
+        this.BOOP_STICKER_COMMAND = new BotCommand(
+                "boop_sticker",
+                "🇮🇹 per ricevere uno sticker casuale di un 𝒹𝑜𝑔𝑔𝑜 🦮\n" +
+                        "🇬🇧🇺🇸 to get a random 𝒹𝑜𝑔𝑔𝑜 sticker"
+        ) {
+            @Override
+            public void execute(AbsSender absSender, User user, Chat chat, String[] strings) {
+
+            }
+        };
+
+        this.BOOP_THE_SNOOT = new BotCommand(
+                "boop_the_snoot",
+                "🇮🇹 per ricevere un 𝒹𝑜𝑔𝑔𝑜 a caso (immagine, GIF o sticker)🐾\n" +
+                        "🇬🇧🇺🇸 to get a random 𝒹𝑜𝑔𝑔𝑜 (pic, GIF, or sticker)"
+        ) {
+            @Override
+            public void execute(AbsSender absSender, User user, Chat chat, String[] strings) {
+
+            }
+        };
     }
 
 
