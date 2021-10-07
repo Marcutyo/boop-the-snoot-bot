90c4e92914617aa2c46e0ce4e90e3e2162e5fc21
Marcutyo <78793610+Marcutyo@users.noreply.github.com>
2021-10-07 18:51:57 +0200

    Fixed formatting

--- src/main/java/it/marcutyo/boopthesnootbot/BoopTheSnootBotApplication.java
+++ src/main/java/it/marcutyo/boopthesnootbot/BoopTheSnootBotApplication.java
@@ -17,8 +17,8 @@ public class BoopTheSnootBotApplication {
     }
 
     @Bean
-    public List<String> stickerSetNames () {
-        List<String> stickerSetNames =  new ArrayList<>();
+    public List<String> stickerSetNames() {
+        List<String> stickerSetNames = new ArrayList<>();
         stickerSetNames.add("BunJoe");
         stickerSetNames.add("Cheemsburbger");
         stickerSetNames.add("bellycorgi");
